485539b8a4a457a549f53829f70339a24bdb4aa3
Thomas Verderesi <tverderesi@gmail.com>
2022-12-20 11:33:25 -0300

    +client: Added Spinner

--- src/client/src/components/Clients.tsx
+++ src/client/src/components/Clients.tsx
@@ -1,10 +1,11 @@
 import { useQuery } from '@apollo/client';
 import ClientRow from './ClientRow';
 import { GET_CLIENTS } from '../queries/clientQueries';
+import Spinner from './Spinner';
 
 export default function Clients() {
   const { loading, error, data } = useQuery(GET_CLIENTS);
-  if (loading) return <p>Loading</p>;
+  if (loading) return <Spinner />;
   if (error) return <p>error</p>;
   return (
     <>
