2c63092730362759f8816678d265cf554c9e98a7
Thomas Verderesi <tverderesi@gmail.com>
2022-12-20 00:19:08 -0300

    +client: Apollo

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,8 +1,19 @@
+import Header from './components/Header';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello World</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <Header />
+      <div className='container'>
+        <h1>Hello World</h1>
+      </div>
+    </ApolloProvider>
   );
 }
 
---
+++ src/client/src/components/Header.tsx
@@ -0,0 +1,23 @@
+//@ts-ignore
+import logo from './assets/logo.png';
+
+export default function Header() {
+  return (
+    <nav className='navbar bg-light mb-4 p-0'>
+      <div className='container'>
+        <a
+          href='/'
+          className='navbar-brand'
+        >
+          <div className='d-flex'>
+            <img
+              src={logo}
+              alt='GraphQL Logo'
+            />
+            <div>ProjectMGMT</div>
+          </div>
+        </a>
+      </div>
+    </nav>
+  );
+}
