f53743606a514606f81be69d330294c759f97da6
Thomas Verderesi <tverderesi@gmail.com>
2022-12-20 13:00:43 -0300

    +clients: new mutation removeClient

--- src/client/src/components/ClientRow.tsx
+++ src/client/src/components/ClientRow.tsx
@@ -1,17 +1,24 @@
 import { FaTrash } from 'react-icons/fa';
-
+import { useMutation } from '@apollo/client';
+import { REMOVE_CLIENT } from '../mutations/clientMutations';
 export default function ClientRow({
   client,
 }: {
-  client: { name: string; email: string; phone: string };
+  client: { name: string; email: string; phone: string; id: string };
 }) {
+  const [removeClient] = useMutation(REMOVE_CLIENT, {
+    variables: { id: client.id },
+  });
   return (
     <tr>
       <td>{client.name}</td>
       <td>{client.email}</td>
       <td>{client.phone}</td>
       <td>
-        <button className='btn btn-danger btn-sm'>
+        <button
+          className='btn btn-danger btn-sm'
+          onClick={removeClient as any}
+        >
           <FaTrash />
         </button>
       </td>
--- src/client/src/mutations/clientMutations.ts
+++ src/client/src/mutations/clientMutations.ts
@@ -1,2 +1,10 @@
 import { gql } from '@apollo/client'
 
+export const REMOVE_CLIENT = gql`mutation removeClient($id: ID!) {
+    removeClient(id: $id) {
+    id
+    name
+    email
+    phone
+}
+}`
