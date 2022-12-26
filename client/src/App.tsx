52dd25f24bdcd2acbb7b254aab865285b3996135
Thomas Verderesi <tverderesi@gmail.com>
2022-12-26 02:53:43 -0300

    Manual Redirect to the server.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -25,7 +25,7 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: `http://localhost:5000/graphql`,
+  uri: `https://project-mgmt-server-vnup.onrender.com/graphql`,
   cache: cache,
 });
 
