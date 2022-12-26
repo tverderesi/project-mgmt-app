3720ab3909747a341a004b55ef83f8e020233caa
Thomas Verderesi <tverderesi@gmail.com>
2022-12-26 01:42:57 -0300

    i've lost it

--- index.ts
+++ index.ts
@@ -22,7 +22,7 @@ app.use(
 if (process.env.NODE_ENV === 'production') {
   //*Set static folder up in production
   app.use(express.static('client/build'));
-//@ts-ignore
+
   app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
 }
 
