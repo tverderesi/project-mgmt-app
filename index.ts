9e913bdcd33d0b07cd8653b54593de21273ceee6
Thomas Verderesi <tverderesi@gmail.com>
2022-12-26 01:35:57 -0300

    test

--- index.ts
+++ index.ts
@@ -22,8 +22,8 @@ app.use(
 if (process.env.NODE_ENV === 'production') {
   //*Set static folder up in production
   app.use(express.static('client/build'));
-
-  app.get('*', (req: any,res: any) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+//@ts-ignore
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
 }
 
 
