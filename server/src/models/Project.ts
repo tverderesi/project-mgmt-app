aa0642f7701a2757bed774e632e8d5ee04f8dbd1
Thomas Verderesi <tverderesi@gmail.com>
2023-12-15 15:12:37 -0300

    Add autoProgress and status fields to Project model

--- server/src/models/Project.ts
+++ server/src/models/Project.ts
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 import { Client } from "./Client";
 import { User } from "./User";
-
+import { Task } from "./Task";
+//TODO: Add Deadlines
 export interface Project {
+  _id?: string;
   name: string;
   description: string;
   client: Client;
+  autoProgress: boolean;
   progress: number;
+  status: "NOT_STARTED" | "IN_PROGRESS" | "COMPLETED";
+  tasks: Task[];
   user: User;
 }
 const projectSchema = new mongoose.Schema<Project>(
@@ -14,12 +19,33 @@ const projectSchema = new mongoose.Schema<Project>(
     name: { type: String, required: true },
     description: { type: String, required: true },
     client: { type: mongoose.Schema.Types.ObjectId, ref: "Client" },
-    progress: { type: Number, default: 0 },
+    autoProgress: { type: Boolean, default: false },
+    status: {
+      type: String,
+      enum: ["NOT_STARTED", "IN_PROGRESS", "COMPLETED"],
+      default: "NOT_STARTED",
+    },
+    progress: { type: Number, default: 0, max: 100, min: 0, transform: Math.round },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }],
   },
   { timestamps: true, autoIndex: true }
 );
 
-const ProjectModel = mongoose.model<Project>("Project", projectSchema);
+projectSchema.pre("save", async function (next) {
+  if (!this.autoProgress) {
+    return next();
+  }
+  const tasks = await mongoose.model("Task").find({ project: this._id, status: "COMPLETED" });
+  const progress = Math.round((tasks.length / this.tasks.length) * 100);
+  this.progress = progress;
+  next();
+});
 
-export default ProjectModel;
+projectSchema.post("findOneAndDelete", async function (next) {
+  const filters = this.getFilter();
+  await mongoose.model("Task").deleteMany({ project: filters._id });
+  next();
+});
+
+export const ProjectModel = mongoose.model<Project>("Project", projectSchema);
