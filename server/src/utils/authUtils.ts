a53be0e907bbbf13f505a7afe54e8ace7178f0b5
Thomas Verderesi <tverderesi@gmail.com>
2023-12-15 12:10:24 -0300

    Add role-based authorization functions

--- server/src/utils/authUtils.ts
+++ server/src/utils/authUtils.ts
@@ -1,12 +1,26 @@
 type Role = "ADMIN" | "USER";
 
-async function checkAuthentication(context: any) {
+export async function checkAuthentication(context: any) {
   const currentUser = await context.getUser();
   if (!currentUser) throw new Error("User not authenticated!");
 }
 
-async function checkAuthorization(context: any, role: Role) {
+export async function checkRoleAuthorization(context: any, role: Role) {
   const currentUser = await context.getUser();
   if (!currentUser) throw new Error("User not authenticated!");
   if (currentUser.role !== role) throw new Error("User not authorized!");
+}
+
+export async function isCurrentUserOrAdmin(context: any, id: string) {
+  const currentUser = await context.getUser();
+  if (!currentUser) throw new Error("User not authenticated!");
+  if (currentUser.role !== "ADMIN" && currentUser.id !== id)
+    throw new Error("User not authorized!");
+  if (currentUser.role !== "ADMIN" && currentUser.id !== id)
+    throw new Error("User not authorized!");
+}
+
+export async function adminViewershipCheck(context, args) {
+  const user = await context.getUser();
+  if (user.role !== "ADMIN") args.userId = user.id;
 }
