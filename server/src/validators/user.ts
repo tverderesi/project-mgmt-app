7c08c7e2777a20cb77e9e66e2611b6378afc8ec1
Thomas Verderesi <tverderesi@gmail.com>
2023-12-15 16:26:48 -0300

    Refactor user validators

--- server/src/validators/user.ts
+++ server/src/validators/user.ts
@@ -2,40 +2,55 @@ import { z } from "zod";
 
 const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,}$/;
 
-export const userInputValidator = z.object({
-  name: z.string({ required_error: "Name is required!" }),
-  username: z.string({ required_error: "Username is required!" }),
-  email: z.string({ required_error: "E-mail is required!" }).email({ message: "Invalid e-mail!" }),
-  confirmEmail: z
-    .string({ required_error: "Confirm E-mail is required!" })
-    .email({ message: "Invalid e-mail!" }),
-  password: z
-    .string({ required_error: "Password is required!" })
-    .regex(
-      passwordRegex,
-      "Password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character"
-    ),
-  confirmPassword: z
-    .string({ required_error: "Confirm Password is required!" })
-    .regex(
-      passwordRegex,
-      "Password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character"
-    ),
+export const userValidator = z.object({
+  _id: z.string().optional(),
+  name: z.string().optional(),
+  email: z.string().email().optional(),
+  username: z.string().optional(),
+  deletedAt: z.string().datetime().or(z.boolean()).optional(),
+  projects: z.array(z.string()).optional(),
+  clients: z.array(z.string()).optional(),
+  role: z.string().optional(),
+  limit: z.number().optional(),
+  skip: z.number().optional(),
+  sort: z.string().optional(),
 });
 
-export const updateUserInputValidator = userInputValidator
-  .partial()
-  .omit({ confirmEmail: true, confirmPassword: true })
-  .extend({
-    oldPassword: z.string({ required_error: "Old Password is required!" }),
-    id: z.string({ required_error: "Id is required!" }),
+export const createUserValidator = userValidator
+  .omit({
+    _id: true,
+    deletedAt: true,
+    projects: true,
+    clients: true,
   })
-  .refine((data) => data.password !== data.oldPassword, {
-    message: "New password must be different from old password!",
-    path: ["password"],
-  });
+  .required({
+    name: true,
+    email: true,
+    username: true,
+  })
+  .extend({
+    name: z.string({ required_error: "Name is required!" }),
+    username: z.string({ required_error: "Username is required!" }),
+    email: z.string({ required_error: "E-mail is required!" }).email({
+      message: "Invalid e-mail!",
+    }),
+    password: z
+      .string({ required_error: "Password is required!" })
+      .regex(
+        passwordRegex,
+        "Password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character."
+      ),
 
-export const createUserInputValidator = userInputValidator
+    confirmPassword: z
+      .string({ required_error: "Confirm Password is required!" })
+      .regex(
+        passwordRegex,
+        "Password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character."
+      ),
+    confirmEmail: z
+      .string({ required_error: "Confirm E-mail is required!" })
+      .email({ message: "Invalid e-mail!" }),
+  })
   .refine((data) => data.email === data.confirmEmail, {
     message: "E-mails don't match!",
     path: ["confirmEmail"],
@@ -45,15 +60,16 @@ export const createUserInputValidator = userInputValidator
     path: ["confirmPassword"],
   });
 
-export const userQueryValidator = z.object({
-  id: z.string().optional(),
-  name: z.string().optional(),
-  email: z.string().email().optional(),
-  deletedAt: z.string().datetime().or(z.boolean()).optional(),
-  projects: z.array(z.string()).optional(),
-  clients: z.array(z.string()).optional(),
-  role: z.string().optional(),
-  limit: z.number().optional(),
-  skip: z.number().optional(),
-  sort: z.string().optional(),
-});
+export const updateUserValidator = userValidator
+  .required({ _id: true })
+  .extend({
+    oldPassword: z.string({ required_error: "Password is required!" }),
+    password: z.string({ required_error: "Password is required!" }).regex(passwordRegex, {
+      message:
+        "Password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character.",
+    }),
+  })
+  .refine((data) => data.password !== data.oldPassword, {
+    message: "New password must be different from old password!",
+    path: ["password"],
+  });
