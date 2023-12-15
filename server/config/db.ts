3b38ce504e6e33b6ec121ab5fd7643682843310c
Thomas Verderesi <tverderesi@gmail.com>
2023-12-15 12:09:07 -0300

    Add error handling for missing Mongo URI

--- server/config/db.ts
+++ server/config/db.ts
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 
 export const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
+  if (!process.env.MONGO_URI) {
+    throw new Error("No Mongo URI found");
+  }
+  const conn = await mongoose.connect(`${process.env.MONGO_URI}`);
+
   console.log(`MongoDB Connected at ${conn.connection.host}:${conn.connection.port}`);
 };
