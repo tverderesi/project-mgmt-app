116b3d50ea8bb33830073d066230c6bd4b383e8f
Thomas Verderesi <tverderesi@gmail.com>
2023-12-16 17:26:14 -0300

    Add ModeToggle component and fix navigation bug

--- vite-client/src/layout/AppLayout.tsx
+++ vite-client/src/layout/AppLayout.tsx
@@ -1,11 +1,14 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { gql, useMutation, useApolloClient, useQuery } from "@apollo/client";
 import { useEffect } from "react";
 import { Button } from "../components/ui/button";
 import { IS_AUTHENTICATED } from "../IS_AUTHENTICATED";
+import { ModeToggle } from "@/components/ui/ModeToggle";
 
 export const AppLayout: React.FC = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
   const client = useApolloClient();
   const LOGOUT = gql`
     mutation Mutation {
@@ -19,7 +22,11 @@ export const AppLayout: React.FC = () => {
     if (error) navigate("../login");
     if (data?.currentUser === null) navigate("../login");
 
-    if (data?.currentUser?.role) navigate(`../app/${data?.currentUser?.role.toLowerCase()}`);
+    if (data?.currentUser?.role) {
+      const href = `/app/${data?.currentUser?.role.toLowerCase()}`;
+      if (!location.pathname.split(href)[1]) navigate(`..${href}`);
+      if (location.pathname.split(href)[1]) navigate(location.pathname);
+    }
   }, [data, error]);
 
   useEffect(() => {
@@ -31,7 +38,8 @@ export const AppLayout: React.FC = () => {
 
   const [logout] = useMutation(LOGOUT);
   return (
-    <div className="h-full w-full">
+    <div className="h-full w-full p-2">
+      <ModeToggle />
       <Button
         variant="outline"
         onClick={() => {
