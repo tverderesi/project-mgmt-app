f78c6f39ec76673b1ebaec6ae2583e9e51d1c60f
Thomas Verderesi <tverderesi@gmail.com>
2023-12-16 01:41:16 -0300

    Add IS_AUTHENTICATED query

---
+++ vite-client/src/IS_AUTHENTICATED.tsx
@@ -0,0 +1,14 @@
+import { gql } from "@apollo/client";
+
+export const IS_AUTHENTICATED = gql`
+  query CurrentUser {
+    currentUser {
+      id
+      name
+      username
+      email
+      password
+      photo
+    }
+  }
+`;
