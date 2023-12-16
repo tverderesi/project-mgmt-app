275a8a2cf4d4ece1c6c8e09d0d4751a8aeafc011
Thomas Verderesi <tverderesi@gmail.com>
2023-12-16 17:26:21 -0300

    Fix formatting and add new utility function

--- vite-client/src/lib/utils.ts
+++ vite-client/src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+export function toTitleCase(str: string) {
+  const words = str.split("_");
+  const capitalizedWords = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+  return capitalizedWords.join(" ");
 }
