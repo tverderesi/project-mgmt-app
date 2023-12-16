c7d74f561e7b93769328b9c33fd405a7c074441b
Thomas Verderesi <tverderesi@gmail.com>
2023-12-16 01:40:25 -0300

    Add AppLayout component

---
+++ vite-client/src/AppLayout.tsx
@@ -0,0 +1,46 @@
+import { Outlet, useNavigate } from "react-router-dom";
+import { gql, useMutation, useApolloClient, useQuery } from "@apollo/client";
+import { useEffect } from "react";
+import { Button } from "./components/ui/button";
+import { IS_AUTHENTICATED } from "./IS_AUTHENTICATED";
+
+export const AppLayout: React.FC = () => {
+  const navigate = useNavigate();
+  const client = useApolloClient();
+  const LOGOUT = gql`
+    mutation Mutation {
+      logout
+    }
+  `;
+
+  const { data, loading, error } = useQuery(IS_AUTHENTICATED);
+
+  useEffect(() => {
+    if (error) navigate("../login");
+    if (data?.currentUser === null) navigate("../login");
+  }, [data, error]);
+
+  useEffect(() => {
+    const unsubscribe = client.onResetStore(() => new Promise(() => navigate("../login")));
+    return () => {
+      unsubscribe();
+    };
+  }, [client]);
+
+  const [logout] = useMutation(LOGOUT);
+  return (
+    <div className="h-full w-full">
+      <Button
+        variant="outline"
+        onClick={() => {
+          logout();
+          client.resetStore();
+        }}
+      >
+        Logout
+      </Button>
+      {loading && <div>Loading...</div>}
+      <Outlet />
+    </div>
+  );
+};
