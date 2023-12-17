cfbfce2f8fd86e4a6e2c20a13c6792051278a2c9
Thomas Verderesi <tverderesi@gmail.com>
2023-12-17 19:54:27 -0300

    Add GraphQL mutations and remove USER query

---
+++ vite-client/src/graphql/mutations.ts
@@ -0,0 +1,41 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN = gql`
+  mutation Login($input: LoginInput!) {
+    login(input: $input) {
+      id
+      name
+      username
+      email
+      photo
+      role
+    }
+  }
+`;
+export const LOGOUT = gql`
+  mutation Mutation {
+    logout
+  }
+`;
+export const SIGN_UP = gql`
+  mutation CreateUser($input: UserInput!) {
+    createUser(input: $input) {
+      id
+      name
+      username
+      email
+      photo
+      role
+    }
+  }
+`;
+
+export const CREATE_CLIENT = gql`
+  mutation CreateClient($input: ClientInput!) {
+    createClient(input: $input) {
+      phone
+      name
+      email
+    }
+  }
+`;
