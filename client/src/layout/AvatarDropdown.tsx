8a583bceec78850e28bacf7f470995f989ef2ae1
tverderesi <tverderesi@gmail.com>
2023-12-21 01:07:45 -0300

    Add AvatarDropdown component

---
+++ client/src/layout/AvatarDropdown.tsx
@@ -0,0 +1,39 @@
+import { useSuspenseQuery } from "@apollo/client";
+import { CURRENT_USER } from "@/graphql/queries/user";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { formatName } from "@/lib/utils";
+import { LogoutButton } from "../components/auth/LogoutButton";
+
+export const AvatarDropdown = () => {
+  const {
+    data: {
+      currentUser: { photo, name },
+    },
+  } = useSuspenseQuery(CURRENT_USER);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar className="float-right">
+          {photo && <AvatarImage src={`data:image/png;base64,${photo}`} />}
+          <AvatarFallback>{name?.split("")[0]}</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" alignOffset={5} className="mt-1">
+        <DropdownMenuLabel>Hi, {formatName(name)}!</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <LogoutButton />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
