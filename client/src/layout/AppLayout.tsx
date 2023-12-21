566d9784de69228d06cacfed407879da28bbb8ca
tverderesi <tverderesi@gmail.com>
2023-12-21 01:07:39 -0300

    Fix bug in login functionality

--- client/src/layout/AppLayout.tsx
+++ client/src/layout/AppLayout.tsx
@@ -1,34 +1,10 @@
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
-import { useMutation, useApolloClient, useQuery, useSuspenseQuery } from "@apollo/client";
-import { Suspense, useEffect } from "react";
-import { USER_STATS, CURRENT_USER } from "../graphql/queries";
-import { ModeToggle } from "@/components/ui/mode-toggle";
-import { LOGOUT } from "@/graphql/mutations";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { formatName, statusDTO } from "@/lib/utils";
-import { Link } from "react-router-dom";
-import { cn } from "@/lib/utils";
-import { forwardRef, ElementRef, ComponentPropsWithoutRef } from "react";
-import { FolderOpen, UserCircle2 } from "lucide-react";
-import { Skeleton } from "@/components/ui/skeleton";
-import { Progress } from "@/components/ui/progress";
+import { useQuery } from "@apollo/client";
+import { useEffect } from "react";
+import { CURRENT_USER } from "@/graphql/queries/user";
+import { Navbar } from "../components/navigation/Navbar";
 import { ErrorBoundary } from "react-error-boundary";
+
 export const AppLayout: React.FC = () => {
   const navigate = useNavigate();
   const location = useLocation();
@@ -39,230 +15,25 @@ export const AppLayout: React.FC = () => {
   });
 
   useEffect(() => {
-    if (error) navigate("../login");
-    if (data?.currentUser === null) navigate("../login");
+    if (error || data?.currentUser) navigate("../login");
+
     if (data?.currentUser?.role) {
-      const href = `/app/${data?.currentUser?.role.toLowerCase()}`;
-      if (!location.pathname.split(href)[1]) navigate(`..${href}`);
-      if (location.pathname.split(href)[1]) navigate(location.pathname);
+      const href = `/app/${data?.currentUser?.role?.toLowerCase()}`;
+      if (location.pathname !== href) navigate(href);
     }
   }, [data, error]);
 
   return (
     <div className="h-full w-full p-2 lg:px-4 relative">
-      {loading && (
-        <div className="absolute inset-0 bg-background/80 backdrop-blur flex items-center justify-center">
-          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-primary"></div>
-        </div>
-      )}
-      {data && <Navbar />}
-      <Outlet />
-    </div>
-  );
-};
-
-const AvatarDropdown = () => {
-  const client = useApolloClient();
-  const navigate = useNavigate();
-  useEffect(() => {
-    const unsubscribe = client.onResetStore(() => new Promise(() => navigate("../login")));
-    return () => {
-      unsubscribe();
-    };
-  }, [client]);
-  const {
-    data: { currentUser },
-    error,
-  } = useSuspenseQuery(CURRENT_USER);
-  const [logout] = useMutation(LOGOUT);
-
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Avatar className="float-right">
-          {currentUser?.photo && <AvatarImage src={`data:image/png;base64,${currentUser?.photo}`} />}
-          <AvatarFallback>{currentUser?.name?.split("")[0]}</AvatarFallback>
-        </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" alignOffset={5} className="mt-1">
-        <DropdownMenuLabel>Hi, {formatName(currentUser?.name)}!</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-
-        <DropdownMenuItem
-          onClick={() => {
-            logout();
-            client.resetStore();
-          }}
-        >
-          Logout
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
-};
-
-const ListItem = forwardRef<ElementRef<typeof Link>, ComponentPropsWithoutRef<typeof Link>>(
-  ({ className, title, children, ...props }, ref) => {
-    return (
-      <li>
-        <NavigationMenuLink asChild>
-          <Link
-            ref={ref}
-            className={cn(
-              "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-              className
-            )}
-            {...props}
-          >
-            <div className="text-sm font-semibold leading-none">{title}</div>
-            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">{children}</p>
-          </Link>
-        </NavigationMenuLink>
-      </li>
-    );
-  }
-);
-ListItem.displayName = "ListItem";
-function Navbar() {
-  const { data } = useQuery(CURRENT_USER);
-  const userRolePath = `./${data?.currentUser?.role?.toLowerCase()}`;
-  return (
-    <div className="fixed z-30 top-0 bg-background/80 -mx-4 border-b border-bottom border-border shadow-sm backdrop-blur w-screen h-16 flex flex-row items-center justify-between px-8">
-      <ModeToggle />
-      <Suspense fallback={<div>Loading...</div>}>
-        <NavigationMenuItem asChild className="font-bold ml-2">
-          <Link to={`./${userRolePath}`}>mgmt.app</Link>
-        </NavigationMenuItem>
-        <div className="hidden  flex-grow max-w-full justify-center items-center md:flex flex-row">
-          <NavigationMenu>
-            <NavigationMenuList>
-              <ProjectNavigationItem data={data} userRolePath={userRolePath} />
-              <ClientNavigationItem data={data} userRolePath={userRolePath} />
-              <TaskNavigationItem data={data} userRolePath={userRolePath} />
-            </NavigationMenuList>
-          </NavigationMenu>
-        </div>
-      </Suspense>
       <ErrorBoundary fallback={<div>Something went wrong</div>}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <AvatarDropdown />
-        </Suspense>
+        {loading && (
+          <div className="absolute inset-0 bg-background/80 backdrop-blur flex items-center justify-center w-screen h-screen z-50">
+            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-primary" />
+          </div>
+        )}
+        <Navbar />
+        <Outlet />
       </ErrorBoundary>
     </div>
   );
-}
-Navbar.displayName = "Navbar";
-function ProjectNavigationItem({ data, userRolePath }: { data: any; userRolePath: string }) {
-  return (
-    <NavigationMenuItem>
-      <NavigationMenuTrigger className="font-semibold">Projects</NavigationMenuTrigger>
-      <NavigationMenuContent>
-        <ul className="grid gap-3 p-4 w-72 md:w-[400px] lg:w-[500px] lg:grid-cols-[.5fr_1fr]">
-          <li className="row-span-3 rounded-md flex flex-col items-center justify-center">
-            <Suspense fallback={<Skeleton className=" w-full h-full" />}>
-              <ProjectCountArray id={data?.currentUser?.id} />
-            </Suspense>
-          </li>
-          <ListItem to={`${userRolePath}/projects/new`} title="New Project">
-            Create a new project.
-          </ListItem>
-          <ListItem to={`${userRolePath}/projects`} title="Projects">
-            Browse and manage all projects.
-          </ListItem>
-        </ul>
-      </NavigationMenuContent>
-    </NavigationMenuItem>
-  );
-}
-
-function ProjectCountArray({ id }: { id: string }) {
-  const { data } = useSuspenseQuery(USER_STATS, {
-    variables: { userStatsId: id },
-  });
-  return (
-    <li className="row-span-1 lg:row-span-3 lg:p-3 lg:bg-rose-500 pl-3 rounded-md dark:lg:text-primary-foreground flex  flex-row lg:flex-col items-center justify-start lg:justify-center w-full h-full">
-      <FolderOpen className="lg:h-16 lg:w-16 mr-2 lg:mr-0" />
-      <span className="font-semibold text-sm md:text-lg lg:mt-1">{data.userStats.projectCount} Projects</span>
-    </li>
-  );
-}
-
-function ClientNavigationItem({ data, userRolePath }: { data: any; userRolePath: string }) {
-  return (
-    <NavigationMenuItem>
-      <NavigationMenuTrigger className="font-semibold">Clients</NavigationMenuTrigger>
-      <NavigationMenuContent>
-        <ul className="grid gap-3 p-4 w-72 md:w-[400px] lg:w-[500px] lg:grid-cols-[.5fr_1fr]">
-          <li className="row-span-3 rounded-md flex flex-col items-center justify-center">
-            <Suspense fallback={<Skeleton className=" w-full h-full" />}>
-              <UserCountArray id={data?.currentUser?.id} />
-            </Suspense>
-          </li>
-          <ListItem to={`${userRolePath}/clients/new`} title="New Client">
-            Register a new client.
-          </ListItem>
-          <ListItem to={`${userRolePath}/clients`} title="Clients">
-            Browse and manage all clients.
-          </ListItem>
-        </ul>
-      </NavigationMenuContent>
-    </NavigationMenuItem>
-  );
-}
-
-function UserCountArray({ id }: { id: string }) {
-  const { data } = useSuspenseQuery(USER_STATS, {
-    variables: { userStatsId: id },
-  });
-  return (
-    <li className="row-span-1 lg:row-span-3 lg:p-3 lg:bg-rose-500 pl-3 rounded-md dark:lg:text-primary-foreground flex  flex-row lg:flex-col items-center justify-start lg:justify-center w-full h-full">
-      <UserCircle2 className="lg:h-16 lg:w-16 mr-2 lg:mr-0" />
-      <span className="font-semibold text-sm md:text-lg lg:mt-1">
-        {data.userStats.clientCount} Client{data.userStats.clientCount !== 1 && "s"}
-      </span>
-    </li>
-  );
-}
-
-function TaskNavigationItem({ data, userRolePath }: { data: any; userRolePath: string }) {
-  return (
-    <NavigationMenuItem>
-      <NavigationMenuTrigger className="font-semibold">Tasks</NavigationMenuTrigger>
-      <NavigationMenuContent>
-        <ul className="grid gap-3 p-4 w-72 md:w-[400px] lg:w-[500px] lg:grid-cols-[.5fr_1fr]">
-          <li className="row-span-3 rounded-md flex flex-col items-center justify-center">
-            <Suspense fallback={<Skeleton className=" w-full h-full" />}>
-              <TaskCountArray id={data?.currentUser?.id} />
-            </Suspense>
-          </li>
-          <ListItem to={`${userRolePath}/tasks/new`} title="New Task">
-            Register a new Task.
-          </ListItem>
-          <ListItem to={`${userRolePath}/clients`} title="Tasks">
-            Browse and manage all Tasks.
-          </ListItem>
-        </ul>
-      </NavigationMenuContent>
-    </NavigationMenuItem>
-  );
-}
-
-function TaskCountArray({ id }: { id: string }) {
-  const { data } = useSuspenseQuery(USER_STATS, {
-    variables: { userStatsId: id },
-  });
-  return (
-    <li className="row-span-1 lg:row-span-3 py-2 pb-3 lg:bg-rose-500 rounded-md dark:lg:text-primary-foreground flex  flex-row lg:flex-col items-center justify-start lg:justify-center w-full h-full">
-      <div className="font-semibold text-sm lg:mt-1 flex flex-col items-center justify-around w-full h-full">
-        {data.userStats.taskCount.map((task: any) => (
-          <div className="space-y-2 w-full px-2">
-            <span>
-              {task.count} {statusDTO(task.status)}
-            </span>
-            <Progress key={task.id} value={Math.round(task.count / data.userStats.totalTaskCount)} className="w-full" />
-          </div>
-        ))}
-      </div>
-    </li>
-  );
-}
+};
