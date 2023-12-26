3ba69ad9a0c1acc7e4818f12f76999b7d7bd3283
Thomas Verderesi <tverderesi@gmail.com>
2023-12-26 15:46:21 -0300

    Refactor client resolvers

--- server/src/graphql/resolvers/client.ts
+++ server/src/graphql/resolvers/client.ts
@@ -1,106 +1,97 @@
 import { ClientModel } from "@/models/Client";
 import { z } from "zod";
-import { clientValidator, createClientValidator, updateClientValidator } from "@/validators/client";
+import clientV from "@/validators/client";
 import { checkAuthentication, isCurrentUserOrAdmin, adminViewershipCheck, checkRoleAuthorization } from "@/utils/auth";
 import { checkRequiredFields } from "@/utils/field";
 import { UserModel } from "@/models/User";
+
 const query = {
-  clients: async (parent: any, args: Partial<z.infer<typeof clientValidator>>, context) => {
-    try {
-      await checkAuthentication(context);
-      await adminViewershipCheck(context, args);
-      const { skip = Number(process.env.DEFAULT_SKIP), limit = Number(process.env.DEFAULT_LIMIT), sort, ...rest } = args;
-      if (skip === undefined || !limit) throw new Error("Skip and limit are required on the environment variables!");
+  clients: async (_parent, args: Partial<z.infer<typeof clientV.query>>, context) => {
+    await checkAuthentication(context);
+    await adminViewershipCheck(context, args);
 
-      const clients = await ClientModel.find(rest).limit(limit).skip(skip).sort(sort);
+    const { skip = Number(process.env.DEFAULT_SKIP), limit = Number(process.env.DEFAULT_LIMIT), sort, ...rest } = args;
+    if (skip === undefined || !limit) throw new Error("Skip and limit are required on the environment variables!");
+    const clients = await ClientModel.find(rest).limit(limit).skip(skip).sort(sort);
 
-      return clients;
-    } catch (error) {
-      throw new Error(error.message);
-    }
+    return clients;
   },
-  client: async (_parent: any, { id }: { id: string }, context) => {
-    try {
-      await checkAuthentication(context);
-      const client = await ClientModel.findById(id);
-      if (!client || client === null) throw new Error("Client not found!");
-      return client;
-    } catch (error) {
-      throw new Error(error.message);
-    }
+
+  client: async (_parent, { id }: { id: string }, context) => {
+    await checkAuthentication(context);
+
+    const client = await ClientModel.findById(id);
+    if (!client || client === null) throw new Error("Client not found!");
+
+    return client;
   },
-  deletedClients: async (parent: any, args: Partial<z.infer<typeof clientValidator>>, context) => {
-    try {
-      await checkAuthentication(context);
-      await adminViewershipCheck(context, args);
-      const { skip = Number(process.env.DEFAULT_SKIP), limit = Number(process.env.DEFAULT_LIMIT) } = args;
-      if (skip === undefined || !limit) throw new Error("Skip and limit are required on the environment variables!");
-      const clients = await ClientModel.find({ deletedAt: { $ne: null } })
-        .limit(limit)
-        .skip(skip);
-      return clients;
-    } catch (error) {
-      throw new Error(error.message);
-    }
+
+  deletedClients: async (_parent, args: Partial<z.infer<typeof clientV.query>>, context) => {
+    await checkAuthentication(context);
+    await adminViewershipCheck(context, args);
+
+    const { skip = Number(process.env.DEFAULT_SKIP), limit = Number(process.env.DEFAULT_LIMIT) } = args;
+    if (skip === undefined || !limit) throw new Error("Skip and limit are required on the environment variables!");
+
+    const clients = await ClientModel.find({ deletedAt: { $ne: null } })
+      .limit(limit)
+      .skip(skip);
+
+    return clients;
   },
 };
 
 const mutation = {
-  createClient: async (parent: any, { input }: { input: z.infer<typeof createClientValidator> }, context) => {
-    try {
-      await checkAuthentication(context);
-      await isCurrentUserOrAdmin(context, input.user);
+  createClient: async (_parent, { input }: { input: z.infer<typeof clientV.create> }, context) => {
+    await checkAuthentication(context);
+    await isCurrentUserOrAdmin(context, input.user);
+    checkRequiredFields(input, clientV.create);
 
-      checkRequiredFields(input, createClientValidator);
+    const client = await ClientModel.create(input);
+    await UserModel.findByIdAndUpdate(input.user, { $push: { clients: client._id } });
 
-      const client = await ClientModel.create(input);
-      await UserModel.findByIdAndUpdate(input.user, { $push: { clients: client._id } });
-
-      return client;
-    } catch (error) {
-      throw new Error(error.message);
-    }
+    return client;
   },
-  updateClient: async (parent: any, args: z.infer<typeof updateClientValidator>, context) => {
-    try {
-      await checkAuthentication(context);
-      await isCurrentUserOrAdmin(context, args.user);
-      const updatedClient = ClientModel.findByIdAndUpdate(args.id, args, { new: true });
-      return updatedClient;
-    } catch (error) {
-      throw new Error(error.message);
-    }
+
+  updateClient: async (_parent, args: z.infer<typeof clientV.update>, context) => {
+    await checkAuthentication(context);
+    await isCurrentUserOrAdmin(context, args.user);
+
+    const updatedClient = ClientModel.findByIdAndUpdate(args.id, args, { new: true });
+
+    return updatedClient;
   },
-  deleteClient: async (parent: any, { _id }: { _id: string }, context) => {
+
+  deleteClient: async (_parent, { _id }: { _id: string }, context) => {
     try {
       await checkAuthentication(context);
       await isCurrentUserOrAdmin(context, _id);
+
       const deletedClient = await ClientModel.findByIdAndUpdate(_id, { deletedAt: new Date() });
+
       return deletedClient;
     } catch (error) {
       throw new Error(error.message);
     }
   },
-  restoreClient: async (parent: any, { id }: { id: string }, context) => {
-    try {
-      await checkAuthentication(context);
-      await isCurrentUserOrAdmin(context, id);
-      const restoredClient = await ClientModel.findByIdAndUpdate(id, { deletedAt: null });
-      return restoredClient;
-    } catch (error) {
-      throw new Error(error.message);
-    }
+
+  restoreClient: async (_parent, { id }: { id: string }, context) => {
+    await checkAuthentication(context);
+    await isCurrentUserOrAdmin(context, id);
+
+    const restoredClient = await ClientModel.findByIdAndUpdate(id, { deletedAt: null });
+
+    return restoredClient;
   },
-  purgeClient: async (parent: any, { id }: { id: string }, context) => {
-    try {
-      await checkAuthentication(context);
-      await checkRoleAuthorization(context, "ADMIN");
-      //TODO: Implment project update to remove client from projects or delete projects
-      const purgedClient = await ClientModel.findByIdAndDelete(id);
-      return purgedClient;
-    } catch (error) {
-      throw new Error(error.message);
-    }
+
+  purgeClient: async (_parent, { id }: { id: string }, context) => {
+    await checkAuthentication(context);
+    await checkRoleAuthorization(context, "ADMIN");
+
+    //TODO: Implment project update to remove client from projects or delete projects
+
+    const purgedClient = await ClientModel.findByIdAndDelete(id);
+    return purgedClient;
   },
 };
 
