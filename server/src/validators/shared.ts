574a2316743c8ee4661231d09a1a996cd870b933
Thomas Verderesi <tverderesi@gmail.com>
2023-12-26 17:11:31 -0300

    Add Enum type for roles and statuses

--- server/src/validators/shared.ts
+++ server/src/validators/shared.ts
@@ -4,6 +4,10 @@ export const queryPaginationParams = {
   skip: z.number().optional(),
   sort: z.string().optional(),
 };
+export type Enum<T extends readonly any[]> = T[number];
+
 export const statuses = ["NOT_STARTED", "IN_PROGRESS", "COMPLETED", "ARCHIVED", "CANCELLED", "OVERDUE", "ON_HOLD"] as const;
-export type Status = (typeof statuses)[number];
 export const roles = ["ADMIN", "USER"] as const;
+
+export type Role = Enum<typeof roles>;
+export type Status = Enum<typeof statuses>;
