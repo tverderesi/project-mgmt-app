7489dbef19730575d244bb54ed3eb6b895eef929
tverderesi <tverderesi@gmail.com>
2024-01-18 23:44:14 -0300

    Refactor TaskCountWidget component

--- apps/client/src/features/navigation/widgets/TaskCountWidget.tsx
+++ apps/client/src/features/navigation/widgets/TaskCountWidget.tsx
@@ -12,7 +12,7 @@ export function TaskCountWidget({ fragmentRef }: { fragmentRef: userTaskCountByS
     <span className="row-span-1 lg:row-span-3 lg:p-3 lg:bg-rose-500 rounded-md dark:lg:text-primary-foreground flex  flex-row lg:flex-col items-center justify-start lg:justify-center w-full h-full">
       <div className="font-semibold text-sm lg:mt-1 flex flex-col items-center justify-around w-full h-full">
         {taskCount.map((task) => (
-          <div className="space-y-2 w-full px-2">
+          <div className="space-y-2 w-full px-2" key={task.status}>
             <span>
               {task.count} {statusDTO(task.status)}
             </span>
