859314829822a5b4490c016f07a7303a005df5af
Thomas Verderesi <tverderesi@gmail.com>
2024-01-24 12:44:42 -0300

    properly typing the fragments.

--- apps/client/src/pages/Dashboard.tsx
+++ apps/client/src/pages/Dashboard.tsx
@@ -1,5 +1,7 @@
 import { ClientsSection } from "@/features/dashboard/client/ClientsSection";
 import { ProjectsSection } from "@/features/dashboard/project/ProjectsSection";
+import { userClient_client$key } from "@/graphql/queries/__generated__/userClient_client.graphql";
+import { userProject_project$key } from "@/graphql/queries/__generated__/userProject_project.graphql";
 import { userUserQuery } from "@/graphql/queries/__generated__/userUserQuery.graphql";
 import { USER } from "@/graphql/queries/user";
 import { useSetPageTitle } from "@/lib/useSetPageTitle";
@@ -8,15 +10,13 @@ import { useLazyLoadQuery } from "react-relay";
 export const Dashboard = () => {
   useSetPageTitle("mgmt.app - Dashboard");
 
-  const { user } = useLazyLoadQuery<userUserQuery>(USER, { id: "" }, { fetchPolicy: "network-only" });
-  const projects = user.projects;
-  const clients = user.clients;
+  const { user } = useLazyLoadQuery<userUserQuery>(USER, { id: "" });
+  const projects = user.projects as userProject_project$key;
+  const clients = user.clients as userClient_client$key;
 
   return (
     <div className="pt-18 space-y-4 pb-4">
-      {/* @ts-ignore */}
       <ProjectsSection fragmentRef={projects} />
-      {/* @ts-ignore */}
       <ClientsSection fragmentRef={clients} />
     </div>
   );
